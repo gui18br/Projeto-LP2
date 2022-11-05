42b7adf9dcebe5ba2ac44b66c8f92216d12bf2b9
guiga18br <guilhermenovaislimaa@hotmail.com>
2022-11-05 20:43:10 -0300

    Primeiro Commit

---
+++ src/Ferrovia/Locomotiva.java
@@ -0,0 +1,24 @@
+package Ferrovia;
+
+public class Locomotiva {
+
+	private char locomotiva;
+	
+	public Locomotiva() {
+		
+	}
+
+	public Locomotiva(char locomotiva) {
+		super();
+		this.locomotiva = locomotiva;
+	}
+
+	public char getLocomotiva() {
+		return locomotiva;
+	}
+
+	public void setLocomotiva(char locomotiva) {
+		this.locomotiva = locomotiva;
+	}
+		
+}
---
+++ src/Ferrovia/Terminal.java
@@ -0,0 +1,16 @@
+package Ferrovia;
+
+import java.util.Stack;
+
+public class Terminal {
+
+	public static void main(String[] args) {
+		Vagao v = new Vagao('a', 1, 22.5, "ferro");
+		Locomotiva l = new Locomotiva('1');
+		Stack<Object> viaFerreaMin = new Stack<>();
+		Stack<Object> viaFerreaGra = new Stack<>();
+		
+		
+	}
+
+}
